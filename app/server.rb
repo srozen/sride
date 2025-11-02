6fb496434e7ffa62981a9369a0331457c9e38902
Samuel Monroe <spat.monroe@gmail.com>
2025-11-02 07:29:42 +0100

    Respond to PING

--- app/server.rb
+++ app/server.rb
@@ -6,12 +6,14 @@ class YourRedisServer
   end
 
   def start
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    puts("Logs from your program will appear here!")
-
-    # Uncomment the code below to pass the first stage
     server = TCPServer.new(@port)
-    client = server.accept
+    loop do
+      client = server.accept
+      while (_line = client.gets.chomp)
+        puts _line
+        client.puts("+PONG\r\n")
+      end
+    end
   end
 end
 
