651c9b981b4e99440c33e027ec4692ac8a6be4f5
Samuel Monroe <spat.monroe@gmail.com>
2025-11-02 08:00:29 +0100

    Iteration over all command parts

--- app/server.rb
+++ app/server.rb
@@ -9,9 +9,21 @@ class YourRedisServer
     server = TCPServer.new(@port)
     loop do
       client = server.accept
-      while (_line = client.gets)
-        puts _line
-        client.puts("+PONG\r\n")
+      while (_line = client.gets.chomp)
+        begin
+          command = []
+          params_number = _line.match(/^\*(\d)/)[1].to_i
+          params_number.times do
+            _bytes = client.gets.chomp
+            _command = client.gets.chomp
+            command.push(_command)
+          end
+          puts("Answering to #{command.inspect}")
+          client.puts("+PONG\r\n")
+        rescue
+          puts 'Unrecognized command, exiting...'
+          client.close
+        end
       end
     end
   end
